d7b54e327445c4772e83696502d600b9e443470a
ZijieZha0 <zz3589@nyu.edu>
2024-02-22 20:05:55 -0500

    setup basic frontend&backend without db

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,29 @@
+import {Card} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+import Rating from './Rating';
+
+
+const Product = props => {
+    return (
+        <Card className='my-3 p-3 rounded'>
+            <Link to={`/product/${props.product._id}`}>
+                <Card.Img src={props.product.image} variant='top' className='card-img'/>
+            </Link>
+            <Card.Body>
+                <Link to={`/product/${props.product._id}`}>
+                    <Card.Title as='div' className='product-title'>
+                        <strong>{props.product.name}</strong>
+                    </Card.Title>
+                </Link>
+                <Card.Text as='div'>
+                    <Rating value={props.product.rating} text={`${props.product.numReviews} reviews`} />
+                </Card.Text>
+                <Card.Text as='h3'>
+                    ${props.product.price}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default Product;
