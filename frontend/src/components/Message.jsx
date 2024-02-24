76758e07f7aeb2c5e1ff066ff8a9bab34fcbd4e6
ZijieZha0 <zz3589@nyu.edu>
2024-02-24 00:33:56 -0500

    replace useState/useEffect with Redux

---
+++ frontend/src/components/Message.jsx
@@ -0,0 +1,15 @@
+import { Alert } from "react-bootstrap";
+
+const Message = (props) => {
+    return (
+        <Alert variant={props.variant}>
+            {props.children}
+        </Alert>
+    )
+};
+
+Message.defaultProps = {
+    variant: 'info'
+};
+
+export default Message;
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,29 +2,40 @@ import  {useParams} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap';
 import Rating from '../components/Rating';
-import axios from 'axios';
-import {useState, useEffect} from 'react';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+// import axios from 'axios';
+// import {useState, useEffect} from 'react';
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice';
 
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState([]);
-
+    // const [product, setProduct] = useState([]);
     const {id:productId} = useParams();
     // const product = products.find((p) => p._id === productId);
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const {data} = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-        fetchProduct();
-    }, [productId]);
+    // useEffect(() => {
+    //     const fetchProduct = async () => {
+    //         const {data} = await axios.get(`/api/products/${productId}`);
+    //         setProduct(data);
+    //     }
+    //     fetchProduct();
+    // }, [productId]);
+
+    const {data:product, isLoading, error} = useGetProductDetailsQuery(productId);
 
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
                 Go Back
             </Link>
-            <Row>
+
+            {isLoading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>
+                    {error?.data?.message || error.error}
+                </Message>
+            ) : (<Row>
                 <Col md={5}>
                     <Image src={product.image} alt={product.name} fluid />
                 </Col>
@@ -75,7 +86,7 @@ const ProductScreen = () => {
                         </ListGroup>
                     </Card>
                 </Col>
-            </Row>
+            </Row>)}
         </>
     )
 }
